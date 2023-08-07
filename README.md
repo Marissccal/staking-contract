2724e95e18f41f963a3db08f79bb5ce6d25f01b5
Mariscal <explotacionesalfa@gmail.com>
2023-08-07 12:31:02 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,31 @@
+## DEPLOYMENTS
+
+### **Deploy Staking Contract**
+
+```bash
+https://devnet-explorer.multiversx.com/accounts/erd1qqqqqqqqqqqqqpgqm9gnr9wa4pcwld2c3c0jhpfws905kwc222uqv5qkr7
+```
+
+### **Stake**
+
+```bash
+https://devnet-explorer.multiversx.com/transactions/9eafdfe98f917325daa58c600f753d1f906203b9652e5a5adc8b6c7d01e1b804
+```
+
+### **Claim Rewards**
+
+```bash
+https://devnet-explorer.multiversx.com/transactions/3872a014f334bf2861dd9147a24adc02fe25f947de05881edfa555693c9d2e98
+```
+
+### **Unstake**
+
+```bash
+https://devnet-explorer.multiversx.com/transactions/fc2851e4bd2247ee79c236016536230d282fcfb83d43042f8215f329f9c7d634
+```
+
+## **TEST**
+
+- **Image**
+    
+<img src="https://gateway.pinata.cloud/ipfs/QmeE4E87kntxFqLJdHNsKvBr2PUvFptrFwRYkKh6u1MiNM">
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           12
+// Endpoints:                           11
 // Async Callback (empty):               1
-// Total number of exported functions:  14
+// Total number of exported functions:  13
 
 #![no_std]
 #![feature(lang_items)]
@@ -27,7 +27,6 @@ multiversx_sc_wasm_adapter::endpoints! {
         contractCreationBlock => contract_creation_block
         contractCreationTimestamp => contract_creation_timestamp
         getStakeAmount => get_stake_amount
-        getTotalRewardsGenerated => get_total_rewards_generated
         getUpdatedTotalRewards => get_updated_total_rewards
         getStakedAddresses => staked_addresses
         getStakingPosition => staking_position
